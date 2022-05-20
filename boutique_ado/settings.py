4424a0bf8dd5c97b41997b609fb087bfcbd4874b
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-05-20 14:12:42 +0000

    056 deployment part 2

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-hqss#61nkg30&ts9x(!#^82h5%g@gfy-389u0#$6m8%%a@9(2n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['preet-boutique-ado.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,12 +122,18 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 
