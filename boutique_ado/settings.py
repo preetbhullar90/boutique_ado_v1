1ab4b55334a742515d95e4974f182f8ab17951cc
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-05-20 21:51:19 +0000

    test

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -206,7 +206,7 @@ if 'USE_AWS' in os.environ:
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
-
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 FREE_DELIVERY_THRESHOLD = 50
