969c7c6e480b27b79744491527c07aa855bbf2a5
preetbhullar90 <gurpreet.singhb90@gmail.com>
2022-05-20 18:36:53 +0000

    Added cache control

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-
 ALLOWED_HOSTS = ['preet-boutique-ado.herokuapp.com', 'localhost']
 
 
